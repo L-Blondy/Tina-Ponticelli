3fc4148e69a36ab08d3f77a02d675110087c9e40
Laurent Blondy <laurent.blondy1@gmail.com>
2019-11-17 17:59:58 +0100

    play with lines now

--- src/main.js
+++ src/main.js
@@ -9,16 +9,6 @@ const sectionContainer = document.querySelector( ".section-container" );
 setScrollNav( sections, sectionContainer )
 
 const moltitudine = document.querySelector( ".moltitudine" )
-const moltitudineItems = new Array( 184 );
-
-for ( let i = 0; i < moltitudineItems.length; i++ ) {
-	const img = document.createElement( "img" );
-	img.className = "moltitudine-item";
-	img.dataset.asset = "moltitudine/" + i;
-	moltitudineItems[ i ] = img;
-	moltitudine.appendChild( img )
-}
-loadImg( moltitudineItems )
 
 const options = {
 	frequency: 1,
