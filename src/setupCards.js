3ce0f3b37e254921e89122685e9e82fb9a11dde7
Laurent Blondy <laurent.blondy1@gmail.com>
2019-12-03 10:43:13 +0100

    setup cards

--- src/setupCards.js
+++ src/setupCards.js
@@ -1,12 +1,13 @@
 export default function setupCards () {
-	const cardsContainer = document.querySelector( ".cards.container" );
+	const cardsContainer = document.querySelector( ".card-container" );
 	const cards = require( "./assets/cards/*.*" );
 
 	for ( let cardName in cards ) {
+
 		const a = document.createElement( "A" );
 		const img = document.createElement( "IMG" );
-		a.className = "card__link";
-		img.className = "card__img";
+		a.className = "card-link";
+		img.className = `card-image ${ cardName.includes( "-lg" ) ? "card-image__lg" : cardName.includes( "-xl" ) ? "card-image__xl" : "" }`;
 		img.dataset.src = "cards/" + cardName;
 		img.setAttribute( "alt", cardName );
 		img.setAttribute( "loadOnScroll", "" );
