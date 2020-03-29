75fdf9a8ad8f18811af00c8d70accde3def7b19b
Laurent Blondy <laurent.blondy1@gmail.com>
2020-03-29 14:52:55 +0200

    4

--- src/JS/Profile.js
+++ src/JS/Profile.js
@@ -1,46 +1,46 @@
-import { text, language } from "./utils/switch_lang.js"
+import { text, language } from "./utils/switch_lang.js";
 
-const btn = document.createElement( "button" )
-const wrap = document.createElement( "div" )
-const en = document.createElement( "div" )
-const it = document.createElement( "div" )
+const btn = document.createElement("button");
+const wrap = document.createElement("div");
+const en = document.createElement("div");
+const it = document.createElement("div");
 
-btn.className = "switch-lang__btn en"
-wrap.className = "switch-lang__btn__wrap"
-en.className = "switch-lang__btn__wrap__lang en"
-it.className = "switch-lang__btn__wrap__lang it"
+btn.className = "switch-lang__btn en";
+wrap.className = "switch-lang__btn__wrap";
+en.className = "switch-lang__btn__wrap__lang en";
+it.className = "switch-lang__btn__wrap__lang it";
 
-en.textContent = "EN"
-it.textContent = "IT"
+en.textContent = "EN";
+it.textContent = "IT";
 
-wrap.appendChild( en )
-wrap.appendChild( it )
-btn.appendChild( wrap )
+wrap.appendChild(en);
+wrap.appendChild(it);
+btn.appendChild(wrap);
 
 export const Profile = {
-	description: document.querySelector( ".profile__article.switch-lang" ),
-	btn: btn.cloneNode( true ),
-	init () {
-		this.addListener()
-
+	description: document.querySelector(".profile__article.switch-lang"),
+	btn: btn.cloneNode(true),
+	init() {
+		this.addListener();
+		this.reloadHTML();
 	},
-	reloadHTML () {
-		this.description.innerHTML = text.profile[ language.cur ]
-		this.btn = btn.cloneNode( true )
-		if ( language.cur == "it" ) {
-			this.btn.classList.toggle( "en" )
-			this.btn.classList.toggle( "it" )
+	reloadHTML() {
+		this.description.innerHTML = text.profile[ language.cur ];
+		this.btn = btn.cloneNode(true);
+		if (language.cur == "it") {
+			this.btn.classList.toggle("en");
+			this.btn.classList.toggle("it");
 		}
-		this.addListener()
+		this.addListener();
 	},
-	addListener () {
-		this.description.appendChild( this.btn )
-		this.btn.addEventListener( "click", () => {
-			this.switch_lang()
-			this.reloadHTML()
-		} )
+	addListener() {
+		this.description.appendChild(this.btn);
+		this.btn.addEventListener("click", () => {
+			this.switch_lang();
+			this.reloadHTML();
+		});
 	},
-	switch_lang () {
-		language.switch()
+	switch_lang() {
+		language.switch();
 	}
-}
+};
--- src/JS/utils/switch_lang.js
+++ src/JS/utils/switch_lang.js
@@ -4,6 +4,7 @@ export const text = {
 		<br/><br/>The peculiarity of Tina Ponticelli's work is her ability in different levels: the first one is chromatic-geometric kind with progressive colors' distances that follow one another along;
 		the second one is symbolic kind with a series of signs, again composed through different combinations between shapes and colors, with some rare exegetic suggestions turned in main point in every single section of the assemblage;
 		the third one has a stylistic quality: if we consider the single frames as freestanding works, we find the whole way of research of the artist, with her abstract and more seldom figurative outcomes that, all together, represent her characteristic dualism.`,
+
 		it: `L’opera “Moltitudine” si presenta come un pannello formato da 184 singole tele, di ridotte dimensioni, tasselli di un maestoso quadro d’insieme, ma anche singole opere finite per forma, stile e contenuti. 
 		<br/><br/>La particolarità del lavoro di Tina Ponticelli è la sua fruibilità a diversi livelli: il primo è di tipo cromatico-geometrico con progressivi stacchi di colori che si susseguono; 
 		il secondo è di tipo simbolico con una serie di segni, anche questi realizzati attraverso diverse combinazioni tra forme e colori, con qualche raro suggerimento esegetico celato quale caposaldo in ogni singola sezione dell’assemblaggio; 
@@ -15,6 +16,7 @@ export const text = {
 		<br/><br/>The artist does not present a single work, but many different works, for how many different perspectives the installation will be observed from.
 		<br/><br/>A work in progress, therefore, for which the exhibition moment does not come, as usual, at the end of a path but the exhibition becomes only the beginning of a further creative phase: the initial time joins the final one
 		<br/><br/>`,
+
 		it: `Con Comunione dei beni, Tina Ponticelli introduce il concetto dell’interattività prospettica, teso ad assegnare allo spettatore un ruolo attivo nella creazione della stessa attraverso una sua diretta partecipazione.
 		<br/><br/>La trasparenza dei supporti di plastica accresce la mutevolezza dell’istallazione che nei singoli elementi oscillanti, trova uno sfondo sempre diverso perché ciascun pezzo, rendendosi visibile attraverso l’elemento che lo precede, ne diventa parte integrante e viceversa. 
 		<br/><br/>L’artista non presenta un'unica opera, ma tante opere diverse, per quante saranno le diverse prospettive dalle quali sarà osservata l’installazione.
@@ -22,18 +24,31 @@ export const text = {
 		<br/><br/>`
 	},
 	profile: {
-		en: "english text here",
-		it: "italiano qua"
+		en: `Tina Ponticelli studied pictorial arts at the "Filippo Palizzi" State Art Institute and later at the Academy of <i>Fine Arts in Naples</i>, where she graduated and specialized with honors in painting.
+		<br/><br/>The artist works mainly using pictorial techniques, the result of careful research, with her abstract and more rarely figurative outcomes which, together, represent her characteristic dualism. She makes use of different means, suitable for her expressive versatility, such as photography and sculpture. 
+		The <i>Touch and create</i> work belongs to the latter modality, present in the permanent collection of the <b>CAM - Contemporary Art Museum</b> of Casoria, Naples, created and acquired on the occasion of the <b>Body</b> museum collective, in 2007. In the same year, the <b>Costa Crociere</b> company selects Ponticelli to furnish the Vip Lounge, in the headquarters of the Neapolitan city, with the pictorial project <i>I cinque Mari</i>, currently visible at the Maritime Palace..
+		<br/><br/>She exhibits alongside the stylist <i>Elio Fiorucci</i> at the Naples Chamber of Commerce, on the occasion of an event on the relationship between fashion and art with a remarkable coverage in the press and television. In addition, she exhibits at the national <b>Abitaly</b> exhibition, at the Mediterranean Fair in Palermo, with the participation of <i>Santo Versace</i>.
+		<br/>In 2011, for the event <b>May of Monuments</b>, she proposed the interactive installation <i>La comunione dei beni</i>.
+		<br/><br/>Several magazines and television programs have talked about her, including: <b>Il Mattino, Il Giornale di Napoli, Il Tempo, Casa Mia, Exibart, MAV (Virtual Archaeological Museum), TG3 Sicily.</b>
+		<br/><br/>Currently the artist works in Italy and in Europe. `,
+
+		it: `Tina Ponticelli studia arti pittoriche presso l’Istituto Statale d’Arte “Filippo Palizzi” e in seguito all’Accademia di <b>Belle Arti di Napoli</b>, dove consegue Laurea e Specializzazione in Pittura con il massimo dei voti.
+        <br/><br/>L’artista lavora avvalendosi prevalentemente di tecniche pittoriche, frutto di un’accurata ricerca, con i suoi esiti astratti e più raramente figurativi che, insieme, rappresentano il suo caratteristico dualismo. Ricorre all’ausilio di mezzi diversi, adatti alla sua versatilità espressiva, quali fotografia e scultura. A quest’ultima modalità appartiene l’opera <i>Touch and create</i>, presente nella collezione permanente del <b>CAM - Contemporary Art Museum</b> di Casoria, Napoli, realizzata e acquisita in occasione della collettiva museale <b>Body</b>, nel 2007. Nello stesso anno, la compagnia <b>Costa Crociere</b> seleziona la Ponticelli per arredare la sala Vip Lounge, nella sede della città partenopea, con il progetto pittorico <i>I cinque Mari</i>, attualmente visibile al Palazzo Marittimo.
+        <br/><br/>Espone al fianco dello stilista <i>Elio Fiorucci</i> presso la Camera di Commercio di Napoli, in occasione di un evento sul rapporto tra moda e arte dal notevole seguito di stampa e televisione. Inoltre, alla mostra nazionale <b>Abitaly</b>, presso la Fiera del Mediterraneo di Palermo, a partecipazione di <i>Santo Versace</i>.
+        <br/>Nel 2011, per la rassegna <b>Maggio dei Monumenti</b>, propone l'installazione interattiva <i>La comunione dei beni</i>.
+        <br/><br/> Hanno parlato di lei diverse riviste e programmi televisivi tra cui emergono: <b>Il Mattino, Il Giornale di Napoli, Il Tempo, Casa Mia, Exibart, MAV (Museo Archeologico Virtuale), TG3 Sicilia.</b>
+        <br/><br/> Attualmente l'artista lavora sul territorio nazionale e all'estero.
+`
 	}
-}
+};
 
 export const language = {
 	en: true,
 	it: false,
-	switch () {
-		[ this.en, this.it ] = [ this.it, this.en ]
+	switch() {
+		[ this.en, this.it ] = [ this.it, this.en ];
 	},
-	get cur () {
-		return this.en ? "en" : "it"
+	get cur() {
+		return this.en ? "en" : "it";
 	}
-}
+};
